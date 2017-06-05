3757d1a31fe3189813c9dd1f13d72a23337d0b2a
Glauber Gomes <glaubergomes@unifei.edu.br>
2017-06-05 01:27:10 -0300

    Alterações na tela de login

--- cadastro.php
+++ cadastro.php
@@ -8,12 +8,21 @@ $nick = $_POST['nick'];
 $tipo = $_POST['tipo'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
-
+$repitasenha = $_POST['repitasenha'];
 
 $query_select = "SELECT nick FROM usuario WHERE nick = '$nick'";
 $select = mysql_query($query_select, $connect);
 $array = mysql_fetch_array($select);
 $logaarray = $array['nick'];
+
+
+if ($repitasenha != $senha) {
+
+    echo "<script language='javascript' type='text/javascript'>alert('ATENÇÃO - As senhas não correspondem!');window.location.href='registrar.php';</script>";
+
+
+}
+
 
 if ($nick == "" || $nick == null) {
     echo "<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='registrar.php';</script>";
@@ -34,3 +43,4 @@ if ($nick == "" || $nick == null) {
         }
     }
 }
+
