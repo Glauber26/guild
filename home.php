2248f63111fd9b34cfde81a9c3a1fa2ff9d96fd9
Glauber Gomes <glaubergomes@unifei.edu.br>
2017-06-04 21:19:09 -0300

    Navegação

--- home.php
+++ home.php
@@ -1,56 +1,27 @@
 <?php
 session_start();
-if(isset($_SESSION['user'])){
-	// Está logado
-	
+if (isset($_SESSION['user'])) {
+    // Está logado
+
 } else {
-	// Não está logado
-	$url = '/guild/login.php';
-	header("location:$url");
+    // Não está logado
+    $url = '/guild/login.php';
+    header("location:$url");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-	<link rel="stylesheet" href="css/style.css">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
 
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Guild Manager</a>
-			</div>
+<?php require_once "navbar.php" ?>
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['user']['nome'] ?><span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/guild/consulta_usuario.php">Editar perfil</a></li>
-							<li><a href="/guild/logout.php">Sair</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
-
-	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-	<script src="js/bootstrap.js"></script>
-	<script src="js/index.js"></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/index.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,138 +1,68 @@
 <?php
 session_start();
-if(isset($_SESSION['user'])){
-	// Está logado
-	$url = '/guild/home.php';
-	header("location:$url");
+if (isset($_SESSION['user'])) {
+    // Está logado
+    $url = '/guild/home.php';
+    header("location:$url");
 } else {
-	// Não está logado
+    // Não está logado
 }
 ?>
 
 
 <!DOCTYPE html>
-<html >
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Guld Manager - Login/Cadastre-se</title>
-	<link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-
-
-	<link rel="stylesheet" href="css/style.css">
-
-
+    <meta charset="UTF-8">
+    <title>Guld Manager - Login/Cadastre-se</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 
 <body>
-	<div class="form">
+<br>
+<br>
+<div class="form">
+    <div class="container col-sm-8 col-sm-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Guild Manager - Fazer Login</div>
+            <div class="panel-body">
+                <div id="login">
 
-		<ul class="tab-group">
-			<li class="tab active"><a href="#signup">Cadastre - se</a></li>
-			<li class="tab"><a href="#login">Acesse sua conta</a></li>
-		</ul>
+                    <form class="form-horizontal" action="login.php" method="post">
 
-		<div class="tab-content">
-			<div id="signup">   
-				<h1>Guild Manager - Cadastre se</h1>
+                        <div class="form-group">
+                            <label for="nick" class="col-sm-2 control-label">Nick</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="nick" name="nick" placeholder="Nick"
+                                       required="required">
+                            </div>
+                        </div>
 
-				<form action="cadastro.php" method="post">
+                        <div class="form-group">
+                            <label for="senha" class="col-sm-2 control-label">Senha</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="senha" name="senha"
+                                       placeholder="Insira uma senha" value="">
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" id="salvar" name="salvar" class="btn btn-default">Entrar</button>
+                                <a class="btn" href="registrar.php" style="float: right">Ainda não tem uma conta? Cadastre-se aqui</a>
+                            </div>
+                        </div>
 
-					<div class="top-row">
-						<div class="field-wrap">
-							<label>
-								Nome<span class="req">*</span>
-							</label>
-							<input type="text" name="nome" required autocomplete="off" />
-						</div>
+                    </form>
 
+                </div>
+            </div>
+        </div>
 
-						<div class="field-wrap">
-							<label>
-								Sobrenome<span class="req">*</span>
-							</label>
-							<input type="text" name="sobrenome" required autocomplete="off"/>
-						</div>
-					</div>
-
-
-					<div class="field-wrap">
-						<label>
-							Nick<span class="req">*</span>
-						</label>
-						<input type="text" name="nick" required autocomplete="off" />
-					</div>
-
-
-					<div class="field-wrap">
-						<label>
-							Tipo de usuário<span class="req">*</span>
-						</label>
-						<input type="text" name="tipo" required autocomplete="off" />
-					</div>
-
-
-					<div class="field-wrap">
-						<label>
-							Email<span class="req">*</span>
-						</label>
-						<input type="email" name="email" required autocomplete="off"/>
-					</div>
-
-					<div class="field-wrap">
-						<label>
-							Insira uma senha<span class="req">*</span>
-						</label>
-						<input type="password" name="senha" required autocomplete="off"/>
-					</div>
-
-					<div class="field-wrap">
-						<label>
-							Repita sua senha<span class="req">*</span>
-						</label>
-						<input type="password" name="repitasenha" required autocomplete="off"/>
-					</div>
-
-
-					<button type="submit" class="button button-block"/>Cadastre - se</button>
-
-				</form>
-
-			</div>
-
-			<div id="login">   
-				<h1>Bem vindo!</h1>
-
-				<form action="login.php" method="post">
-
-					<div class="field-wrap">
-						<label>
-							Nick<span class="req">*</span>
-						</label>
-						<input type="text" name="nick" required autocomplete="off"/>
-					</div>
-
-					<div class="field-wrap">
-						<label>
-							Password<span class="req">*</span>
-						</label>
-						<input type="password" name="senha" required autocomplete="off"/>
-					</div>
-
-					<p class="forgot"><a href="#">Esqueceu a senha?</a></p>
-
-					<button class="button button-block"/>Entrar</button>
-
-				</form>
-
-			</div>
-
-		</div><!-- tab-content -->
-
-	</div> <!-- /form -->
-	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-	<script src="js/index.js"></script>
+    </div>
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/index.js"></script>
 
 </body>
 </html>
