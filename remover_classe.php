e19836d288e01ccb319e9863745cbeec8b76416e
Glauber Gomes <glaubergomes@unifei.edu.br>
2017-06-05 17:12:37 -0300

    Alterações no remover classe

--- remover_classe.php
+++ remover_classe.php
@@ -6,7 +6,19 @@ $query = "DELETE FROM classe WHERE nome='" . $_GET['nome'] . "'";
 $insert = mysql_query($query, $connect);
 
 $url = '/guild/classe.php';
+
 header("location:$url");
+/*
+if ($insert) {
+    $sucesso = 'Classe removida com sucesso!';
+} else {
+    $erro = 'Não foi possível remover essa classe, pois a mesma está em uso por um personagem';
+}
+*/
+
+
+
+
 
 ?>
 
